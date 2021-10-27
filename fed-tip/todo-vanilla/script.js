9325584eb37b28bd1a85e5154345fa578bdc1a7a
aman-maharshi <amanm4harshi@gmail.com>
2021-10-27 11:46:48 +0530

    edit functionality complete

--- fed-tip/todo-vanilla/script.js
+++ fed-tip/todo-vanilla/script.js
@@ -4,25 +4,30 @@ const todoInput = document.querySelector(".todo-input")
 const todoList = document.querySelector(".list")
 
 const allTodoItems = []
+let editTodoIndex = null
+let editElement = null
 
 // FUNCTIONS
 const handleFormSubmit = e => {
     e.preventDefault()
     const userInput = todoInput.value
 
-    if (userInput) {
+    if (userInput && !editTodoIndex) {
         if (allTodoItems.includes(userInput)) {
             console.log("Task already added")
         } else {
+            /*--------- ADDING NEW TODO ITEM ----------*/
             allTodoItems.push(userInput)
             const todoItemElement = document.createElement("li")
             todoItemElement.textContent = userInput
 
+            // creating edit button
             const editBtn = document.createElement("button")
             editBtn.classList.add("editBtn")
             editBtn.textContent = "Edit"
             todoItemElement.appendChild(editBtn)
 
+            // creating delete button
             const xBtn = document.createElement("button")
             xBtn.classList.add("close")
             xBtn.textContent = "X"
@@ -30,7 +35,18 @@ const handleFormSubmit = e => {
 
             todoList.appendChild(todoItemElement)
         }
-    } else {
+    } else if (userInput && editTodoIndex) {
+        /*------- EDITING EXISTING TODO ITEM ------------*/
+        // Update the DOM
+        editElement.childNodes[0].nodeValue = userInput
+
+        // Update the allTodoItems Array
+        allTodoItems[editTodoIndex] = userInput
+
+        // Resetting the edit variables
+        editTodoIndex = null
+        editElement = null
+    } else if (!userInput) {
         console.log("Please enter a todo Item")
     }
 
@@ -40,28 +56,29 @@ const handleFormSubmit = e => {
 
 const handleTodoDelete = e => {
     if (e.target.classList.contains("close")) {
-        // Remove item from DOM
+        // Update the DOM
         const itemToDelete = e.target.parentElement
         itemToDelete.remove()
 
-        // Remove item from Array
+        // Remove item from allTodoItems Array
         const itemTextContent = e.target.parentElement.textContent
         const todoItem = itemTextContent.substring(0, itemTextContent.length - 5)
         const itemIndex = allTodoItems.indexOf(todoItem)
         allTodoItems.splice(itemIndex, 1)
 
-        // console the updated Array
+        // console the updated allTodoItems Array
         console.log(allTodoItems)
     }
 }
 
 const handleTodoEdit = e => {
     if (e.target.classList.contains("editBtn")) {
-        console.log("edit click")
+        editElement = e.target.parentElement
+        const todoItem = e.target.parentElement.childNodes[0].nodeValue
+        todoInput.value = todoItem
 
-        // Update item from Array
-
-        // console the updated Array
+        const itemIndex = allTodoItems.indexOf(todoItem)
+        editTodoIndex = itemIndex
     }
 }
 
